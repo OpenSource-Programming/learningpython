e8e89770c9dd72f64f38171909b972b26dd9523c
DesertOwl2397 <jgwill1947@gmail.com>
2020-09-20 20:00:40 -0700

    Add files via upload

---
+++ johnnyw_py_wk1_exercises.py
@@ -0,0 +1,16 @@
+# Exercise 2
+
+name = input("Enter your name:\n")
+print("Hello " + name)
+
+# Exercise 3
+
+hours = float(input('Enter hours worked: '))
+payrate = float(input('Enter pay rate: '))
+print('Pay is: ' + str(hours * payrate))
+
+# Exercise 5
+
+Celsius = float(input('Enter Celsius temperature: '))
+Fahrenheit = 1.8 * Celsius + 32
+print('Fahrenheit temperature is ' + str(Fahrenheit))
