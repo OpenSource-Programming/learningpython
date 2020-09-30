ca55e4de0c9669af96ba8c33716db5ff356d88b1
aguileratech <62417769+aguileratech@users.noreply.github.com>
2020-09-29 22:44:03 -0700

    Add files via upload

---
+++ leo_py_week02_ex_03_01_cond.py
@@ -0,0 +1,16 @@
+# Exercise 3.1
+
+hours = input("Enter Hours: ")
+rate = input("Enter Rate: ")
+
+hours = float(hours)
+rate = float(rate)
+
+if hours > 40 :
+    pay_regular = hours * rate
+    pay_overtime = (hours - 40.0) * (rate * 0.5)
+    pay_total = pay_regular + pay_overtime
+else:
+    pay_total = hours * rate
+
+print("Pay:", pay_total)
