07afd0a4be96cd2e7bdfd0126df98863afab80c6
aguileratech <62417769+aguileratech@users.noreply.github.com>
2020-09-29 22:40:26 -0700

    Add files via upload

---
+++ leo_py_week01_ex_02_02_ves.py
@@ -0,0 +1,4 @@
+# Exercise 2.2
+
+name = input("Enter your name: ")
+print("Hello", name)
---
+++ leo_py_week01_ex_02_03_ves.py
@@ -0,0 +1,11 @@
+# Exercise 2.3
+
+hours = input("Enter Hours: ")
+rate = input("Enter Rate: ")
+
+hours = float(hours)
+rate = float(rate)
+
+pay = hours * rate
+
+print("Pay:", pay)
