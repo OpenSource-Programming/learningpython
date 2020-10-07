6596f663eddc2f3084cba055063cd6a49d313775
DesertOwl2397 <jgwill1947@gmail.com>
2020-10-07 12:31:16 -0700

    Add files via upload
    Loops and Iteration

---
+++ johnnyw_py_wk3_ex_5_1.py
@@ -0,0 +1,19 @@
+total = 0.0
+count = 0
+while True:
+    try:
+        number = input('> ')
+        if number == 'done':
+            break
+        fn = int(number)
+        total += fn
+        count += 1
+        continue
+    except:
+        print('Invalid input')
+        continue
+if total > 0:
+    print(str(total) + ' ' + str(count) + ' ' + str(total/count))
+else:
+    print('0 0 0')
+print('Done!')
